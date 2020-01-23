6179e542d018328f0f221c726f7a13f8f013677e
jhilldss <37665325+jhilldss@users.noreply.github.com>
2020-01-23 14:58:58 -0500

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,44 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grade requires a minimum of 5 students");
+
+            var allAveGrades = new List<double>();
+            var StudentCount = Students.Count;
+            var x = StudentCount * .2;
+            int y = (int)x;
+
+            foreach (var student in Students)
+            {
+                allAveGrades.Add(student.AverageGrade);
+            }
+            allAveGrades.Sort();
+            allAveGrades.Reverse();
+
+            switch(averageGrade)
+            {
+                case var d when d >= allAveGrades[(y * 1) - 1]:
+                    return 'A';
+
+                case var d when d >= allAveGrades[(y * 2) - 1]:
+                    return 'B';
+
+                case var d when d >= allAveGrades[(y * 3) - 1]:
+                    return 'C';
+
+                case var d when d >= allAveGrades[(y * 4) - 1]:
+                    return 'D';
+
+                default:
+                    return 'F';
+
+            }
+            
+
+        }
     }
 }
